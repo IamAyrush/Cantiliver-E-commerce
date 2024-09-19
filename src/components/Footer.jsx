f6d23117a3c2f3309e927bd41cb79e1c3448567f
Ayrush Verma <av577294@gmail.com>
2024-09-19 11:16:56 +0530

    Eccomerce Websit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="mb-0 text-center">
+      <footer className="mb-0 text-center bg-dark text-white">
         <div className="d-flex align-items-center justify-content-center pb-5">
-          <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
-            </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+          <div className="col-md-6 mt-5">
+            <p className="mb-3 mb-md-0">Made by @Ayush Verma</p>
+            <a className="text-light fs-4" href="https://github.com/IamAyrush" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
